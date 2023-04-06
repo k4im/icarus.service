846308130aac1cd7d9db56f0d6749a7365eab1ce
Santos <joao@platon.com.br>
2023-04-06 16:46:16 -0300

    Added to sln

--- icarus.estoque/Data/DataContext.cs
+++ icarus.estoque/Data/DataContext.cs
@@ -2,16 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameWorkCore;
-
+using Microsoft.EntityFrameworkCore;
 namespace icarus.estoque.Data
 {
     public class DataContext : DbContext
     {
-
         public DataContext(DbContextOptions options) : base(options)
-        {}
-
-        
+        {
+        }
     }
 }
--- icarus.estoque/Program.cs
+++ icarus.estoque/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using icarus.estoque.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>();
+builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Teste"));
 
 var app = builder.Build();
 
