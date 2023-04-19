df8ed35a5fe3dd5c068b64d963e406ebad44bc2b
k4im <contato.vitorsantos@hotmail.com>
2023-04-19 08:29:58 -0300

    Adequado Docker-compose

--- icarus.gateway/Program.cs
+++ icarus.gateway/Program.cs
@@ -7,26 +7,27 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-var authenticationProviderKey = builder.Configuration["JWt:SecretKey"];
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(authenticationProviderKey, x => {
-    x.Authority = builder.Configuration["Jwt:Issuer"];
-    x.Audience = builder.Configuration["Jwt:Audience"];
-    x.RequireHttpsMetadata = false;
-    x.SaveToken = true;
-    x.Configuration = new OpenIdConnectConfiguration();  //<-- Most IMP Part
-    x.TokenValidationParameters = new TokenValidationParameters{
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
 
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey
-        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
-    };
-});
+// var authenticationProviderKey = builder.Configuration["JWt:SecretKey"];
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+// .AddJwtBearer(authenticationProviderKey, x => {
+//     x.Authority = builder.Configuration["Jwt:Issuer"];
+//     x.Audience = builder.Configuration["Jwt:Audience"];
+//     x.RequireHttpsMetadata = false;
+//     x.SaveToken = true;
+//     x.Configuration = new OpenIdConnectConfiguration();  //<-- Most IMP Part
+//     x.TokenValidationParameters = new TokenValidationParameters{
+//         ValidateIssuer = false,
+//         ValidateAudience = false,
+//         ValidateLifetime = true,
+//         ValidateIssuerSigningKey = true,
+
+//         ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//         ValidAudience = builder.Configuration["Jwt:Audience"],
+//         IssuerSigningKey = new SymmetricSecurityKey
+//         (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
+//     };
+// });
 #region  configuração de ocelot
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, 
