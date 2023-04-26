cae854254e2d98223dd3c6311f775feeceb266bb
k4im <contato.vitorsantos@hotmail.com>
2023-04-26 15:45:07 -0300

    RefreshToken

---
+++ icarus.jwtManager/Migrations/20230426184432_refreshToken.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace icarus.jwtManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class refreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    UserEmail = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TokenRefresh = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.UserEmail);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+        }
+    }
+}
--- icarus.jwtManager/Models/RefreshToken.cs
+++ icarus.jwtManager/Models/RefreshToken.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace icarus.jwtManager.Models
 {
     public class RefreshToken
     {
+
+        [Key]
         public string UserEmail { get; set; }
+        
+        [Required(ErrorMessage = "Campo precisa ser preenchido")]
         public string TokenRefresh { get; set; }
     }
 }
